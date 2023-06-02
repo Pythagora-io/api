2788a5b8a46ed0d3f460aa651e022052eef9718f
LeonOstrez <leon.ostrez@gmail.com>
2023-06-02 15:54:21 +0200

    fix system message in prompt for unit tests

--- helpers/openai.js
+++ helpers/openai.js
@@ -65,9 +65,7 @@ async function getJestTestName(req, res, usedNames) {
 async function getJestUnitTests(req, res, usedNames) {
     return await createGPTChatCompletion([
         {"role": "system", "content": "You are a QA engineer and your main goal is to find ways to break the application you're testing. You are proficient in writing automated tests for Node.js apps.\n" +
-                "When you respond, you don't say anything except the code - no formatting, no explanation - only code.\n" +
-                "\n" +
-                "When you create names for your tests you make sure that they are very intuitive and in a human-readable form." },
+                "When you respond, you don't say anything except the code - no formatting, no explanation - only code." },
         {
             "role": "user",
             "content": getPromptFromFile('generateJestUnitTest.txt', { functionName: req.body.functionName, functionCode: req.body.functionCode, relatedCode: req.body.relatedCode }),
