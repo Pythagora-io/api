bcf28639a6bd7c6ef341845e747b1dfd95b2f57a
LeonOstrez <leon.ostrez@gmail.com>
2023-06-08 10:35:03 +0200

    fix server_boot.bash script

--- server_boot.bash
+++ server_boot.bash
@@ -3,9 +3,9 @@
 cd /home/ubuntu
 
 tmux new-session -d -s pythagora
-tmux send-keys -t safetytest-vue:0 'sudo su ubuntu' C-m
-tmux send-keys -t safetytest-vue:0 'cd /home/ubuntu/api' C-m
-tmux send-keys -t safetytest-vue:0 'git stash' C-m
-tmux send-keys -t safetytest-vue:0 'git pull origin main' C-m
-tmux send-keys -t safetytest-vue:0 'npm i' C-m
-tmux send-keys -t safetytest-vue:0 'npm run start' C-m
+tmux send-keys -t pythagora 'sudo su ubuntu' C-m
+tmux send-keys -t pythagora 'cd /home/ubuntu/api' C-m
+tmux send-keys -t pythagora 'git stash' C-m
+tmux send-keys -t pythagora 'git pull origin main' C-m
+tmux send-keys -t pythagora 'npm i' C-m
+tmux send-keys -t pythagora 'npm run start' C-m
